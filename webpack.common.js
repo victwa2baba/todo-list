6aa4cd753b12b590abb93f41d0befc384031f8c5
videv <vtounaregha@gmail.com>
2025-05-27 12:25:53 +0100

    added prettier and eslint

--- webpack.common.js
+++ webpack.common.js
@@ -1,11 +1,10 @@
 // webpack.config.js
-'use strict'
+"use strict";
 
 const path = require("path");
-const autoprefixer = require('autoprefixer');
+const autoprefixer = require("autoprefixer");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const miniCssExtractPlugin = require('mini-css-extract-plugin');
-
+const miniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   mode: "development",
@@ -28,51 +27,49 @@ module.exports = {
   module: {
     rules: [
       {
-        mimetype: 'image/svg+xml',
-        scheme: 'data',
-        type: 'asset/resource',
+        mimetype: "image/svg+xml",
+        scheme: "data",
+        type: "asset/resource",
         generator: {
-          filename: 'icons/[hash].svg'
-        }
+          filename: "icons/[hash].svg",
+        },
       },
-        {
+      {
         test: /\.s(a|c)ss$/,
         use: [
           {
             // Extracts CSS for each JS file that includes CSS
-            loader: miniCssExtractPlugin.loader
+            loader: miniCssExtractPlugin.loader,
           },
           {
             // Interprets `@import` and `url()` like `import/require()` and will resolve them
-            loader: 'css-loader'
+            loader: "css-loader",
           },
           {
             // Loader for webpack to process CSS with PostCSS
-            loader: 'postcss-loader',
+            loader: "postcss-loader",
             options: {
               postcssOptions: {
-                plugins: [
-                  autoprefixer
-                ]
-              }
-            }
+                plugins: [autoprefixer],
+              },
+            },
           },
           {
             // Loads a SASS/SCSS file and compiles it to CSS
-            loader: 'sass-loader',
+            loader: "sass-loader",
             options: {
               sassOptions: {
                 // Optional: Silence Sass deprecation warnings. See note below.
                 silenceDeprecations: [
-                  'mixed-decls',
-                  'color-functions',
-                  'global-builtin',
-                  'import'
-                ]
-              }
-            }
-          }
-        ]
+                  "mixed-decls",
+                  "color-functions",
+                  "global-builtin",
+                  "import",
+                ],
+              },
+            },
+          },
+        ],
       },
       {
         test: /\.css$/i,
